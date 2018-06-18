ad1e3d656c8dc121a97e3481e47d1a67406748c1
frankcao@me.com Cao <frankcao@me.com>
2018-06-18 20:19:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
-/*
+
 function deleteFromObjectByKey(object, key) {
   const newObj = Object.assign({}, object)
-
   delete newObj[key]
-
   return newObj
 }
-
+/*
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
 

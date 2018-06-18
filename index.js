a68441a3bca2888865b22b42eb7860d46dc21d84
frankcao@me.com Cao <frankcao@me.com>
2018-06-18 20:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,9 @@ function deleteFromObjectByKey(object, key) {
   delete newObj[key]
   return newObj
 }
-/*
+
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
 
   return object
 }
-*/
